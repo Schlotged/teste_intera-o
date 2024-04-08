03c11ac2ce690913b0d1babba4eafb45c808222c
Gedson Schlotefeldt <86088828+Schlotged@users.noreply.github.com>
2024-04-08 11:31:59 -0300

    Add files via upload

--- kanban-main/site/script.js
+++ kanban-main/site/script.js
@@ -50,7 +50,6 @@ function logout() {
 function dragstart() {
     // log('CARD: Start dragging ')
     dropzones.forEach( dropzone => dropzone.classList.add('highlight'));
-
     // this = card
     this.classList.add('is-dragging');
 }
@@ -79,7 +78,8 @@ function dragenter() {
     // log('DROPZONE: Enter in zone ')
 }
 
-function dragover() {
+function dragover(event) {
+
     // this = dropzone
     this.classList.add('over');
 
@@ -88,28 +88,51 @@ function dragover() {
 
     // this = dropzone
     this.appendChild(cardBeingDragged);
+    event.preventDefault();
+
 }
 
 function dragleave() {
     // log('DROPZONE: Leave ')
     // this = dropzone
     this.classList.remove('over');
-
 }
 
-function drop(event) {
-    // Prevent default (opening as link for some elements)
-    event.preventDefault();
-    // Remove 'over' class from dropzone
-    this.classList.remove('over');
-    // Get the dragging card
+function drop() {
     const cardBeingDragged = document.querySelector('.is-dragging');
-    // Append the dragging card to the dropzone
-    this.querySelector('.cards').appendChild(cardBeingDragged);
-    // Remove 'is-dragging' class from the dragging card
     cardBeingDragged.classList.remove('is-dragging');
-    // Adjust the size of all cards in the dropzone to match the first card
-    adjustCardSize(this);
+    
+    this.classList.remove('over');
+
+    const cardDrop = this.querySelector('.card-table'); // Encontra o elemento do cartão dentro da zona de drop
+    const cardId = cardDrop.dataset.cardId; // Obtém o idCard do elemento do cartão
+    const cardType = this.getAttribute('data-type'); // Obtém o tipo da zona de drop
+
+    console.log("ID do card arrastado:", cardId);
+    console.log("Tipo do card:", cardType);
+    console.log("Tentando atualizar o tipo do cartão com ID:", cardId, "e tipo:", cardType);
+
+
+    firebase.firestore()
+        .collection('card')
+        .doc(cardId) // Usar cardId em vez de cardDrop.id
+        .get()
+        .then(doc => {
+            if (doc.exists) {
+                const currentType = doc.data().type;
+                if (currentType !== cardType) {
+                    // Apenas atualize se o tipo atual for diferente do novo tipo
+                    return doc.ref.update({ type: cardType });
+                } else {
+                    console.log("O tipo do cartão não foi alterado. Nenhuma atualização necessária.");
+                }
+            } else {
+                console.error(`Documento com ID ${cardId} não encontrado.`);
+            }
+        })
+        .catch(error => {
+            console.error(`Erro ao atualizar o tipo do cartão ${cardId} no Firebase:`, error);
+        });
 
 }
 
@@ -121,63 +144,82 @@ function logout() {
     })
 }
 
-// const fakeCard = [{
-//     type: 'todo',
-//     title: 'Agendamento Privado',
-//     texto: 'Agendamento pendente para fulano.',
-//     aviso: 'Agendar'
-// }, {
-//     type: 'doing',
-//     title: 'Agendamento Privado',
-//     texto: 'Agendamento feito na clinica tal para fulano.',
-//     aviso: 'Agendandado'
-// }, {
-//     type: 'done',
-//     title: 'Agendamento Privado',
-//     texto: 'Agendamento concluido para fulano.',
-//     date: '2024-02-17',
-//     aviso: 'Concluido',
-
-// }]
-
+const toggleSidebar = () => document.body.classList.toggle("open");
 
 function addCardToScreen(cardData) {
+    cardData.sort((a, b) => new Date(b.date) - new Date(a.date));
     const kanbanTodo = document.getElementById('todo');
     const kanbanDoing = document.getElementById('doing');
     const kanbanDone = document.getElementById('done');
+    
 
-    cardData.forEach(cardData => {
-        const cardDiv = document.createElement('div');
+    cardData.forEach(cardData  => {
+        const cardDiv = document.createElement('table');
         cardDiv.classList.add(cardData.type);
-
-        const cardTitle = document.createElement('h3');
-        cardTitle.textContent = cardData.title;
-
-        const cardP = document.createElement('P');
-        cardP.textContent = cardData.texto;
-
-        const dataspan = document.createElement('span');
-        dataspan.textContent = cardData.date;
-        dataspan.classList.add('data-span');
-
-        const cardSpan = document.createElement('span');
-        cardSpan.textContent = cardData.aviso;
-        cardSpan.classList.add('card-tags');
-        
-        cardDiv.appendChild(cardTitle);
-        cardDiv.appendChild(cardP);
-        cardDiv.appendChild(dataspan);
-        cardDiv.appendChild(cardSpan);
-        
-
         cardDiv.classList.add('card');
+        
 
-        // Adiciona atributos de arrastar e soltar
+        // Add double click event listener
+        cardDiv.addEventListener('dblclick', () => {
+            newCard() + cardDiv.id;
+            console.log(cardData.idCard);
+        });
+
+        // Create table for card data
+        const table = document.createElement('table');
+        table.classList.add('card-table');
+        table.dataset.cardId = cardData.uid;
+
+        // Create table body
+        const tbody = document.createElement('tbody');
+
+        const dataRow = document.createElement('tr');
+
+
+        const titleData = document.createElement('td');
+        titleData.textContent = cardData.title;
+        titleData.style.textAlign = 'left';
+        titleData.style.width = '110px';
+        dataRow.appendChild(titleData);
+
+        const textoData = document.createElement('td');
+        textoData.textContent = cardData.texto;
+        textoData.style.textAlign = 'left';
+        textoData.style.width = '140px';
+
+        dataRow.appendChild(textoData);
+
+        const dateData = document.createElement('td');
+        dateData.textContent = cardData.date;
+        dateData.style.textAlign = 'center';
+        dateData.classList.add('data-span');
+        dateData.style.width = '100px';
+        dataRow.appendChild(dateData);
+
+
+
+        const avisoData = document.createElement('td');
+        avisoData.textContent = cardData.aviso;
+        avisoData.style.textAlign = 'left';
+        avisoData.classList.add('tags');
+        avisoData.style.width = '150px';
+        dataRow.appendChild(avisoData);
+
+        const colaboradorData = document.createElement('td');
+        colaboradorData.textContent = cardData.colaborador;
+        colaboradorData.style.textAlign = 'center';
+        colaboradorData.style.width = '100px';
+        dataRow.appendChild(colaboradorData);
+
+        tbody.appendChild(dataRow);
+        table.appendChild(tbody);
+
+        cardDiv.appendChild(table);
+        // Add draggable attributes
         cardDiv.setAttribute('draggable', true);
         cardDiv.addEventListener('dragstart', dragstart);
         cardDiv.addEventListener('drag', drag);
         cardDiv.addEventListener('dragend', dragend);
-
 
         if (cardData.type === 'todo') {
             kanbanTodo.appendChild(cardDiv);
@@ -186,6 +228,48 @@ function addCardToScreen(cardData) {
         } else if (cardData.type === 'done') {
             kanbanDone.appendChild(cardDiv);
         }
+
+        function diffDays(date1, date2) {
+            const diffTime = Math.abs(date2.getTime() - date1.getTime());
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+            return diffDays;
+        }
+        
+        // Obter a data atual
+        const currentDate = new Date();
+        
+        // Converter a data salva no cartão para um objeto de data
+        const savedDate = new Date(cardData.date);
+        
+        // Calcular a diferença em dias entre a data atual e a data do cartão
+        const daysDifference = diffDays(currentDate, savedDate);
+        
+        // Obter a data de criação do cartão
+        const creationDate = new Date(cardData.creationDate);
+        
+        // Calcular a diferença em dias entre a data atual e a data de criação do cartão
+        const daysSinceCreation = diffDays(currentDate, creationDate);
+        
+        // Verificar se passaram mais de 10 dias desde a criação do cartão e aplicar estilos correspondentes
+        if (daysSinceCreation >= 30) {
+            // Se passaram mais de 10 dias desde a criação do cartão, aplicar o estilo de cor amarela ao cardDiv
+            cardDiv.style.borderLeftColor = 'red';
+            dateData.style.color = 'red';
+
+        } else if (daysDifference >= 10) {
+            // Se passaram mais de 10 dias desde a data do cartão, adicionar a classe 'tags' ao elemento dateData e alterar a cor da borda do cardDiv para vermelho
+            dateData.style.color = '#FF8C00';
+            cardDiv.style.borderLeftColor = '#FF8C00'; 
+
+        } else if (daysDifference >= 5) {
+            // Se passaram mais de 5 dias desde a data do cartão, aplicar o estilo de cor amarela diretamente ao elemento dateData e à borda do cardDiv
+            dateData.style.color = 'green';
+            cardDiv.style.borderLeftColor = 'green';
+        } else {
+            dateData.style.color = 'none';
+            cardDiv.style.borderLeftColor = 'gray';
+        }
+
 
     });
 }
@@ -199,7 +283,10 @@ function findCard(user) {
         .get()
         .then(snapshot => {
 
-            const cardData = snapshot.docs.map(doc => doc.data());
+            const cardData = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                uid: doc.id,
+            }));
             addCardToScreen(cardData);
         })
         .catch(error => {
@@ -226,13 +313,16 @@ function closeModal() {
 }
 
 function addCardToNewCard() {
+
     showLoading();
     const type = document.getElementById('todo').dataset.type;
     const title = document.getElementById('title_card');
     const texto = document.getElementById('text-new-card');
     const date = document.getElementById('data-new-card');
     const aviso  = document.getElementById('avisos-new-card');
+    const colaborador  = document.getElementById('colaborador-new-card');
     const buttonsave = document.getElementById('save-new-card');
+    const idCard = Math.floor(Math.random() * (10000 - 100 + 1)) + 100;
 
     if (!isFormValid()) {
         return false;
@@ -240,15 +330,19 @@ function addCardToNewCard() {
         buttonsave.disabled = false; // Habilita o botão de salvar se o formulário for válido
     }
 
+    const currentDate = new Date(); // Obtem a data atual
     const cardObj = {
         type: type,
         title: title.value,
         texto: texto.value,
         date: date.value,
         aviso: aviso.value,
+        colaborador: colaborador.value,
+        idCard: idCard,
         user: {
             uid: firebase.auth().currentUser.uid
-        }
+        },
+        creationDate: currentDate
         
     };
 
@@ -289,41 +383,9 @@ function isFormValid() {
 }
 
 
-// Função para criar elemento de cartão com base nos dados
-function createCardElement(cardData) {
-    const cardDiv = document.createElement('div');
-    cardDiv.classList.add('card');
-
-    const cardTitle = document.createElement('h3');
-    cardTitle.textContent = cardData.title;
-
-    const cardP = document.createElement('p');
-    cardP.textContent = cardData.texto;
-
-    const dataspan = document.createElement('span');
-    dataspan.textContent = cardData.date;
-    dataspan.classList.add('data-span');
-
-    const cardSpan = document.createElement('span');
-    cardSpan.textContent = cardData.aviso;
-    cardSpan.classList.add('card-tags');
-    
-    cardDiv.appendChild(cardTitle);
-    cardDiv.appendChild(cardP);
-    cardDiv.appendChild(dataspan);
-    cardDiv.appendChild(cardSpan);
-
-    // Adiciona atributos de arrastar e soltar
-    cardDiv.setAttribute('draggable', true);
-    cardDiv.addEventListener('dragstart', dragstart);
-    cardDiv.addEventListener('drag', drag);
-    cardDiv.addEventListener('dragend', dragend);
-
-    return cardDiv;
-}
 
 let lastActivityTime;
-const MAX_INACTIVITY_TIME = 10 * 60 * 1000; // 30 minutos em milissegundos
+const MAX_INACTIVITY_TIME = 60 * 60 * 1000; // 30 minutos em milissegundos
 
 // Função para atualizar o tempo da última atividade
 function updateLastActivityTime() {
@@ -347,3 +409,4 @@ document.addEventListener("keydown", updateLastActivityTime);
 
 // Define um intervalo para verificar periodicamente a inatividade do usuário
 setInterval(checkInactivityAndLogout, 60000); // Verifica a cada minuto
+
